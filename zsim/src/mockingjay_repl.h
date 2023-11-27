315d872dbb1feda4aaf864c39e9e18d7241973e9
User <user@main-10-231-220-66.tamulink.tamu.edu>
2023-11-26 23:12:07 -0600

    Still working on improving performance over LRU

--- zsim/src/mockingjay_repl.h
+++ zsim/src/mockingjay_repl.h
@@ -7,41 +7,49 @@
 #include "limits.h"
 #include "stdlib.h"
 #include <algorithm>
+#include <bitset>
+#include <iostream>
 #include <iterator>
 #include <map>
 #include <set>
-#include <iostream>
 
 class MockingjayReplPolicy: public ReplPolicy {
     protected:
         uint32_t numLines;
         uint32_t ways;
+        uint32_t numOverflows;
         std::set<uint32_t> sampled_sets;
+        bool clock_overflow;
         struct SampledEntry {
             bool valid;
             uint16_t address_tag;
             uint16_t pc_signature;
             uint8_t timestamp;
+            uint8_t overflow;
         };
         std::map<uint32_t, SampledEntry*> sampled_cache;
-        std::map<uint32_t, uint8_t> rdp;
+        std::map<uint16_t, uint8_t> rdp;
         std::map<uint16_t, uint32_t> pc_map;
         uint8_t* etr_counters;
         uint8_t* set_timestamps;
-        //uint8_t* set_clock;
+        uint8_t current_timestamp;
 
         void update_sampled_cache(uint32_t id, const MemReq* req, uint32_t set) {
             if(sampled_sets.find(set) == sampled_sets.end()) {
-                SampledEntry* s = new SampledEntry[128];
-                for(int i = 0; i < 128; i++) {
-                    s[i].valid = false;
+                SampledEntry* s = new SampledEntry[80];
+                for(int i = 0; i < 80; i++) {
+                    s[i] = {false, 0, 0, 0, 0};
                 }
                 sampled_cache[set] = s;
                 sampled_sets.insert(set);
-                std::cout << set << std::endl;
             }
-            
-           set_timestamps[set]++;
+            if(current_timestamp == UINT8_MAX) {
+                clock_overflow = true;
+                numOverflows++;
+            }
+            else   
+                clock_overflow = false;
+            set_timestamps[set] = current_timestamp++;
             //set_clock[set]++;
             /*
             if(sampled_sets.find(set) == sampled_sets.end()) {
@@ -73,25 +81,33 @@ class MockingjayReplPolicy: public ReplPolicy {
             uint16_t tag = (req->lineAddr & 0x03FF0) >> 4;
             bool sampleMiss = true;
             uint8_t pred;
-            for(uint32_t i = sampled_set * 8; i < sampled_set * 8 + 8; i++) {
+            std::cout << "LLC Set #: " << set << std::endl;
+            //std::cout << "LLC Set: " << set << " " << "Sampled Cache Set: " << sampled_set << " " << "Address Tag: " << tag << " " << "Set timestamp: " << +set_timestamps[set] << std::endl;
+            for(uint32_t i = sampled_set * 5; i < sampled_set * 5 + 5; i++) {
+                //std::cout << "Tag: " << sampled_cache[set][i].address_tag << " " << std::boolalpha << sampled_cache[set][i].valid << " " << "Last access timestamp: " << +sampled_cache[set][i].timestamp << std::endl;
                 if(sampled_cache[set][i].valid && sampled_cache[set][i].address_tag == tag) {
                     sampleMiss = false;
-                    if(set_timestamps[set] < sampled_cache[set][i].timestamp)
-                    {
+                    if(clock_overflow)
+                        sampled_cache[set][i].overflow++;
+                    /*
+                    if(set_timestamps[set] < sampled_cache[set][i].timestamp) {
                         uint16_t overflow = (1 << 8) + set_timestamps[set];
-                        pred = (overflow - sampled_cache[set][i].timestamp) > 104 ? 127 : set_timestamps[set] - sampled_cache[set][i].timestamp;;
+                        pred = (overflow - sampled_cache[set][i].timestamp) > 104 ? 127 : set_timestamps[set] - sampled_cache[set][i].timestamp;
                     }
                     else
                         pred = (set_timestamps[set] - sampled_cache[set][i].timestamp) > 104 ? 127 : set_timestamps[set] - sampled_cache[set][i].timestamp;
+                    */
+                    uint16_t temp = (1 << 8) * numOverflows + set_timestamps[set] - (numOverflows - sampled_cache[set][i].overflow) * sampled_cache[set][i].timestamp;
+                    pred = temp > 104 ? 127 : set_timestamps[set] - sampled_cache[set][i].timestamp;
                     sampled_cache[set][i].timestamp = set_timestamps[set];
-                    sampled_cache[set][i].pc_signature = req->pc;
+                    sampled_cache[set][i].pc_signature = req->pc & 0x07FF;
+                    pc_map[sampled_cache[set][i].pc_signature] = id;
                     update_rdp(sampled_cache[set][i].pc_signature, pred, sampleMiss);
                 }
             }
             if(sampleMiss) {
-                uint32_t bestCand = -1;
-                uint32_t bestScore = 128;
-                for(uint32_t i = sampled_set * 8; i < sampled_set * 8 + 8; i++) {
+                bool foundOverflow = false;
+                for(uint32_t i = sampled_set * 5; i < sampled_set * 5 + 5; i++) {
                     if(!sampled_cache[set][i].valid) {
                         sampled_cache[set][i].valid = true;
                         sampled_cache[set][i].address_tag = (req->lineAddr & 0x03FF0) >> 4;
@@ -101,18 +117,32 @@ class MockingjayReplPolicy: public ReplPolicy {
                         return;
                     }
                     else {
-                        uint8_t s = sampled_cache[set][i].timestamp;
-                        if(s > set_timestamps[set] && s < bestScore) {
-                            bestCand = i;
-                            bestScore = s;
-                        }
-                        else {
-                            bestCand = (s < bestScore) ? i : bestCand;
-                            bestScore = MIN(s, bestScore);
-                        }
+                        if(sampled_cache[set][i].timestamp  > set_timestamps[set])
+                            foundOverflow = true;
                     }
                 }
-                update_rdp(sampled_cache[set][bestCand].pc_signature, uint8_t(127), sampleMiss);
+                uint32_t bestCand = -1;
+                uint32_t bestScore = UINT32_MAX;
+                if(foundOverflow) {
+                    uint16_t overflow_timestamps[5];
+                    for(uint32_t i = sampled_set * 5; i < sampled_set * 5 + 5; i++) {
+                        //std::cout << "Timestamp: " << +s << std::endl;
+                        uint32_t j = i % 5;
+                        overflow_timestamps[j] = ((1 << 8) * (numOverflows - sampled_cache[set][i].overflow)) + sampled_cache[set][i].timestamp;
+                        bestCand = (overflow_timestamps[j] < bestScore) ? i : bestCand;
+                        bestScore = MIN(overflow_timestamps[j], bestScore);
+                    }
+                }
+                else {
+                    for(uint32_t i = sampled_set * 5; i < sampled_set * 5 + 5; i++) {
+                        //std::cout << "Timestamp: " << +s << std::endl;
+                        bestCand = (sampled_cache[set][i].timestamp < bestScore) ? i : bestCand;
+                        bestScore = MIN(sampled_cache[set][i].timestamp, bestScore);
+                    }
+                }
+                //std::cout << "Made it here" << std::endl;
+                pred = 127;
+                update_rdp(sampled_cache[set][bestCand].pc_signature, pred, sampleMiss);
                 sampled_cache[set][bestCand].address_tag = (req->lineAddr & 0x03FF0) >> 4;
                 sampled_cache[set][bestCand].pc_signature = req->pc & 0x07FF;
                 sampled_cache[set][bestCand].timestamp = set_timestamps[set];
@@ -122,12 +152,16 @@ class MockingjayReplPolicy: public ReplPolicy {
         }
 
         void update_rdp(uint16_t pc_signature, uint8_t pred, bool miss) {
+            //for (std::map<uint16_t, uint8_t>::iterator it=rdp.begin(); it!=rdp.end(); ++it)
+                //std::cout << it->first << " => " << +it->second << std::endl;
+            //std::cout << '\n';
+            //for (std::map<uint16_t, uint32_t>::iterator it=pc_map.begin(); it!=pc_map.end(); ++it)
+                //std::cout << it->first << " => " << it->second << std::endl;
+            //std::cout << '\n';
             uint32_t id = pc_map[pc_signature];
-            if(miss)
-            {
+            if(miss) {
                 pc_signature <<= 1;
                 rdp[pc_signature] = pred;
-                pc_map[pc_signature] = id;
             }
             else {
                 pc_signature <<= 1;
@@ -138,16 +172,11 @@ class MockingjayReplPolicy: public ReplPolicy {
                     uint8_t old_pred = rdp[pc_signature];
                     int diff = abs(old_pred - pred);
                     float_t w = MIN(1, diff/6);
-                    w = w > 1 ? ceil(w) : floor(w);
                     uint8_t new_pred = (pred > old_pred) ? pred + w : pred - w;
                     rdp[pc_signature] = new_pred > 104 ? 127 : new_pred;
                 }
             }
             etr_counters[id] = rdp[pc_signature];
-        }
-
-        uint16_t hash(uint64_t val) {
-            return (uint16_t)(val & 0xFFFF) ^ (uint16_t)(val >> 16 & 0xFFFF) ^ (uint16_t)(val >> 32 & 0xFFFF) ^ (uint16_t)(val >> 48 & 0xFFFF);
         }
 
     public:
@@ -159,11 +188,12 @@ class MockingjayReplPolicy: public ReplPolicy {
             uint32_t numSets = numLines / ways;
             //set_clock = gm_calloc<uint8_t>(numSets);
             set_timestamps = gm_calloc<uint8_t>(numSets);
+            clock_overflow = false;
+            /*
             std::vector<uint32_t> v;
             for(uint32_t i = 0; i < numSets; i++) {
                 v.push_back(i);
             }
-            /*
             std::random_shuffle(v.begin(), v.end());
             v.resize(32);
             for(std::vector<uint32_t>::iterator it = v.begin(); it != v.end(); ++it) {
@@ -203,12 +233,11 @@ class MockingjayReplPolicy: public ReplPolicy {
         template <typename C> uint32_t rank(const MemReq* req, C cands) {
             uint32_t bestCand = *cands.begin();
             uint32_t bestScore = abs(etr_counters[bestCand]);
+            //uint32_t set = req->lineAddr & ((numLines/ways) - 1);
+            //std::cout << "LLC Set #: " << set << std::endl;
             for (auto ci = cands.begin(); ci != cands.end(); ci.inc()) {
                 uint32_t i = *ci;
-                if(etr_counters[i] == UINT8_MAX) {
-                    bestCand = i;
-                    break;
-                }
+                //std::cout << "LLC Line #: " << *ci << " " << "ETR value: " << abs(etr_counters[i]) << std::endl;
                 if(abs(etr_counters[i]) > bestScore) {
                     bestCand = i;
                     bestScore = abs(etr_counters[i]);
@@ -217,6 +246,7 @@ class MockingjayReplPolicy: public ReplPolicy {
                     bestCand = i;
                 }
             }
+            //std::cout << '\n';
             return bestCand;
         }
         DECL_RANK_BINDINGS
